2defc0717e9b5e6fb2c37362b63e54c8cbadf642
CoDeR300TrUe <108196474+CoDeR300TrUe@users.noreply.github.com>
2022-11-12 10:53:17 +0300

    Update index.js
    Обновление

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-        <p>это мой первый сайт, который находится в интернете</p>
+        <p className="description">это мой первый сайт, который находится в интернете</p>
       </main>
 
       <Footer />
