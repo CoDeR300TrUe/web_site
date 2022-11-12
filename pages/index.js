5f97de74e7660bbe36cc37fe331d3262385faa06
CoDeR300TrUe <108196474+CoDeR300TrUe@users.noreply.github.com>
2022-11-12 20:28:05 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Космос</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
     <body>
       <header>
@@ -45,6 +45,26 @@ export default function Home() {
             </div>
           </article>
         </section>
+        <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+      </form>
       </main>
       <footer>
         <img class="socicon vk" src="assets/footer/vk.png" />
