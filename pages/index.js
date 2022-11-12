a081dc846520a6db53523e7ae1f258bd56baaa6e
CoDeR300TrUe <108196474+CoDeR300TrUe@users.noreply.github.com>
2022-11-12 12:14:00 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,50 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+    <body>
+      <header>
+        <h1>Космос: неизведанный мир</h1>
+        <nav>
+          <a href="#" class="iconup">
+            <img src="assets/header/sputnik.png" alt="" />
+            Солнечная система
+          </a>
+          <a href="#" class="iconup">
+            <img src="assets/header/rocket.png" alt="" />
+            Техника
+          </a>
+          <a href="#" class="iconup">
+            <img src="assets/header/star.png" alt="" />
+            Земные звёзды
+          </a>
+        </nav>
+      </header>
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <p><code>index.js</code></p>
-        <h2>Это мой первый сайт, который находится в интернете:)</h2>
-        <p>проверка связи</p>
+        <section id="system">
+          <h2>Солнечная система</h2>
+          <article>
+            <div class="planet">
+              <img src="assets/planets/venus.jpg" alt="" />
+              <p>Венера</p>
+            </div>
+            <div class="planet">
+              <img src="assets/planets/earth.jpg" alt="" />
+              <p>Земля</p>
+            </div>
+            <div class="planet">
+              <img src="assets/planets/jupiter.jpg" alt="" />
+              <p>Юпитер</p>
+            </div>
+          </article>
+        </section>
       </main>
-
+      <footer>
+        <img class="socicon vk" src="assets/footer/vk.png" />
+        <img class="socicon yt" src="assets/footer/youtube.png" />
+        <img class="socicon tg" src="assets/footer/telegram.png" />
+        <img class="socicon snpcht" src="assets/footer/snapchat.png" />
+      </footer>
+    </body>
       <Footer />
     </div>
   )
